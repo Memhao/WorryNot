076f7bd5949dd32af7227a1536c470a29d22b06d
Memhao <xander.shandor@gmail.com>
2017-06-15 12:13:07 +0300

    Comments with author again

--- app/src/main/java/licence/meme/worrynot/gui/recycleview/RecycleViewItem.java
+++ app/src/main/java/licence/meme/worrynot/gui/recycleview/RecycleViewItem.java
@@ -11,10 +11,8 @@ import licence.meme.worrynot.models.Method;
  *
  */
 public class RecycleViewItem {
-//    private String methodTitle;
-//    private String author;
-//    private int methodImageResourceID;
-    private boolean isFavourite = false;
+
+
 
     private Method method;
     private String key;
@@ -42,35 +40,6 @@ public class RecycleViewItem {
     public Method getMethod(){
         return method;
     }
-//    public String getMethodTitle() {
-//        return methodTitle;
-//    }
-//
-//    public void setMethodTitle(String methodTitle) {
-//        this.methodTitle = methodTitle;
-//    }
-//
-//    public int getMethodImageResourceID() {
-//        return methodImageResourceID;
-//    }
-//
-//    public void setMethodImageResourceID(int methodImageResourceID) {
-//        this.methodImageResourceID = methodImageResourceID;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public void setAuthor(String author) {
-//        this.author = author;
-//    }
-//
-    public boolean isFavourite() {
-        return isFavourite;
-    }
 
-    public void setFavourite(boolean favourite) {
-        isFavourite = favourite;
-    }
+
 }
